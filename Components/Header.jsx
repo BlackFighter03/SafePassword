f50f9753c0b5316458cd693074e689d223d37e4a
BlackFighter03 <alenat.scarpa@gmail.com>
2024-12-24 11:10:07 +0100

    header

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -3,14 +3,13 @@ import { styles } from "./Graphic features";
 import { useState } from "react";
 
 
-export default Header = (headerTxt) => {
-    const [title, setTitle]=useState(headerTxt);
+export default Header = ({headerTxt}) => {
 return(
     <View style={styles.headerBackground}>
     <TouchableOpacity onPress={handleSignInPage} style={styles.iconAreaHeaderLeft}>
       <Ionicons name="return-up-back-outline" style={styles.iconHeaderLeft} onPress={handleSignInPage} />
     </TouchableOpacity>
-    <Text style={styles.textHeader}>{"Recupera password"}</Text>
+    <Text style={styles.textHeader}>{headerTxt}</Text>
   </View>
 );
 };
