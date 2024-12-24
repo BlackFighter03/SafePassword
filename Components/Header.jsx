b33fe5d10665f5b8d6f6c8c116d28581b28cf862
BlackFighter03 <alenat.scarpa@gmail.com>
2024-12-24 11:08:17 +0100

    Creazione modulo header (non completo)

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -6,8 +6,11 @@ import { useState } from "react";
 export default Header = (headerTxt) => {
     const [title, setTitle]=useState(headerTxt);
 return(
- <View style={styles.headerBackground}>
-    <Text style={styles.textHeader}>{title}</Text>
- </View>   
+    <View style={styles.headerBackground}>
+    <TouchableOpacity onPress={handleSignInPage} style={styles.iconAreaHeaderLeft}>
+      <Ionicons name="return-up-back-outline" style={styles.iconHeaderLeft} onPress={handleSignInPage} />
+    </TouchableOpacity>
+    <Text style={styles.textHeader}>{"Recupera password"}</Text>
+  </View>
 );
 };
